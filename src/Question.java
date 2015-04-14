e93fd57a1c6370230d45ae0a14da0c4cfc6e918d
JennGoodman <russus@gmail.com>
2015-04-13 23:47:14 -0400

    Completed Question Class Nearly Completed Quiz Class
    Added: resetCurrentQuestion to Quiz Class.
    Added: questions.txt (For Testing)
    
    ToDo: Quiz - SaveShowResults

--- src/Question.java
+++ src/Question.java
@@ -1,13 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author Jenn
+ * Question Class
+ * 
+ * Generic class to manage and store data about a question.
+ * 
+ * @author Jenn Goodman
  */
 public class Question {
     
+    // Fields //
+    private int qType;
+    private String selectedAnswer;
+    private Integer value;
+    private String question;
+    private Object[][] answers;
+    
+    // Constructors //
+    
+    Question(int type, String question, Object[][] answers) {
+        qType = type;
+        this.question = question;
+        this.answers = answers;
+    }
+    
+    // Methods //
+    
+    public int getType () {
+        return qType;
+    }
+    
+    public String getQuestion () {
+        return question;
+    }
+    
+    public Object[][] getPossibleAnswers () {
+        return answers;
+    }
+    
+    public Object[] getSelectedAnswer () {
+        Object[] temp = new Object[2];
+        temp[0] = selectedAnswer;
+        temp[1] = value;
+        return temp;
+    }
+    
+    public void setSelectedAnswer (String answer, int value) {
+        selectedAnswer = answer;
+        this.value = value;
+    }
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -1,12 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.Integer.getInteger;
+import java.util.ArrayList;
+
 /**
- *
- * @author jngoodma
+ * Quiz Class
+ * 
+ * Generic class for containing and managing a quiz and it's questions.
+ * 
+ * @author Jenn Goodman
  */
 public class Quiz {
-    //no comment
+    
+    // Fields //
+    
+    private ArrayList<Object> questions;
+    private int currentQuestion;
+    private final String path;
+
+    // Constructors //
+    
+    Quiz (String path) throws IOException {
+        this.path = path;
+        
+        // Open File
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String foo;
+            String[] bar;
+            int rows = 0;
+            Object[][] answers = new Object[0][0];
+
+            while ((foo = reader.readLine()) != null) {
+                bar = foo.split(",");
+
+                switch (Integer.getInteger(bar[0])) {
+                    case 1: break;              // Short Answer
+                    case 2: break;              // Drop Down
+                    case 3: rows = 2; break;    // True/False
+                    case 4: rows = 4; break;    // Multiple Choice
+                    case 5: rows = 5; break; }   // Scale
+                
+                if (rows > 0) {
+                    int max = bar.length;
+                    int i = 2;
+                    answers =  new Object[rows][3];
+                    for (int j = 0 ; j < rows ; j++)
+                        for (int k = 0; k < 3 ; k++)
+                            if (i < max) answers[j][k] = bar[i++];
+                }
+                questions.add(new Question(getInteger(bar[0]), bar[1], answers));
+            }
+            reader.close();
+        }
+    }
+    
+    // Methods //
+    
+    public Question getNextQuestion() {
+        if (questions.size() == currentQuestion) return null;
+        return (Question)questions.get(currentQuestion++);
+    }
+    
+    public void resetCurrentQuestion () {
+        currentQuestion = 0;
+    }
+    
+    public void saveShowResults() {
+        // To do
+    }
 }
