a70448fd5e500d0d6b30c0bf27b5237e1f08af0d
charlieswing <charlesswing@gmail.com>
2015-04-17 08:21:08 -0400

    Updated SortingHat main class

--- src/SortingHat.java
+++ src/SortingHat.java
@@ -19,6 +19,7 @@ public class SortingHat extends JFrame{
 		setJMenuBar(theMenuBar);
 		setPreferredSize(new Dimension(300, 500));
 		setSize(new Dimension(300,500));
+		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		//Set up main content panel (which will change depending on what panel is called)
@@ -31,18 +32,60 @@ public class SortingHat extends JFrame{
 		setVisible(true);
 		
 		//Waiting for submit button to be clicked
+		int cnt=0;
 		while(participantPanel.submitClicked()==false){
-			//System.out.println("waiting...");
+			if(cnt<1){
+				System.out.println("");
+				cnt++;
+			}
 		}
-		System.out.println("CLICKED!");
 
 		/*--------------Begin Quiz Questions-------------*/
 		
 		Quiz quiz = new Quiz("questions.txt");
 		Question question = quiz.getNextQuestion();
-		//load question panel and call multiple choice panel
-		setContentPane(new QuestionViewPanel(question));
-		revalidate();
+		QuestionPanel questionPanel = null;
+		//while there are more questions
+		
+		while(question != null){
+			int questionType = question.getType();
+			
+			//load question panel depending on type of question
+			
+			//Multiple Choice
+			switch (questionType) {
+			case 3: // True/False
+				questionPanel = new TrueFalseQuestionPanel(question);
+				setContentPane(questionPanel);
+	            	break;
+	        	case 4: // Multiple Choice
+	        	questionPanel = new MultiChoicePanel(question, false);
+	        	setContentPane(questionPanel);
+	            	break;
+	        	case 5: // Scale
+	        	questionPanel = new ScaleQuestionPanel(question);
+	        	setContentPane(questionPanel); 
+	        	 break;
+	        	case 6: // Tiebreak multiple choice
+	        	questionPanel = new MultiChoicePanel(question, true);
+	        	setContentPane(questionPanel);
+	        	 break;
+			}
+			revalidate(); //sets screen to this next question panel
+			
+			//waits for button to be clicked
+			cnt = 0;
+			while(questionPanel.buttonClicked == false){
+				if(cnt<1){
+					System.out.println("");
+					cnt++;
+				}
+			}
+			
+			//checks for next question
+			question = quiz.getNextQuestion();
+		}
+		
 	}
 	
 	public static void main(String[] args) throws IOException{
